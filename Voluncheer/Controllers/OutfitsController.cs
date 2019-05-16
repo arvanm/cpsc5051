5bd759bbf6a1bbe4ca410dda483db7e3df6cc665
HossCraft3 <marvan@craft3.org>
2019-05-16 12:44:56 -0700

    Add Outfit page

---
+++ Voluncheer/Controllers/OutfitsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Voluncheer.Controllers
+{
+    public class OutfitsController : Controller
+    {
+        // GET: Outfits
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
