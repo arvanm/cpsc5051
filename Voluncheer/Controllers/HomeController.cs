fb543ca441db6dca503b10dded494ef86fb052f8
ahtran7 <32027806+ahtran7@users.noreply.github.com>
2019-05-18 23:32:16 -0700

    Added FAQ Page

--- Voluncheer/Controllers/HomeController.cs
+++ Voluncheer/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Voluncheer.Controllers
             return View();
         }
 
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
