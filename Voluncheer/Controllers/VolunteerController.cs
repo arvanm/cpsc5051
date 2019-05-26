c6327ac107b02fe9558b13ae466d5955506b2be9
Josh R <rubrightjosh@seattleu.edu>
2019-05-26 13:18:05 -0700

    Tied Profile.cshtml View to VolunteerController.cs

--- Voluncheer/Controllers/VolunteerController.cs
+++ Voluncheer/Controllers/VolunteerController.cs
@@ -13,5 +13,11 @@ namespace Voluncheer.Controllers
         {
             return View();
         }
+
+        // GET: Volunteer Profile page
+        public ActionResult Profile()
+        {
+            return View();
+        }
     }
 }
