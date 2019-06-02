edf0ff22e8b9d28661a4d9998da540ef8873f500
Josh R <rubrightjosh@seattleu.edu>
2019-06-02 12:38:08 -0700

    added IOutfitRepository.cs

---
+++ Voluncheer/Backend/IOutfitRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voluncheer.Models;
+
+namespace Voluncheer.Backend
+{
+    interface IOutfitRepository
+    {
+        OutfitModel Create(OutfitModel data);
+        OutfitModel Read(String id);
+        OutfitModel Update(OutfitModel data);
+        Boolean Delete(String id);
+        List<OutfitModel> Index();
+    }
+}
