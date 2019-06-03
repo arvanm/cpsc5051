10ef7b0ffca7afd5ac418433f11149256831e77e
Aaron Wells <chacewells@gmail.com>
2019-06-02 19:04:08 -0700

    Added event repository interface

---
+++ Voluncheer/Backend/IEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voluncheer.Models;
+
+namespace Voluncheer.Backend
+{
+    interface IEventRepository
+    {
+        EventModel Create(EventModel data);
+        EventModel Read(String id);
+        EventModel Update(EventModel data);
+        Boolean Delete(String id);
+        List<EventModel> Index();
+    }
+}
